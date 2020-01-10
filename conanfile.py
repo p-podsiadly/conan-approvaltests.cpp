b6520edd2610a04343592b7ea00c4336b6f8bfda
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-01-10 15:33:11 +0100

    update to 7.0.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class ApprovalTestsCppConan(ConanFile):
     name = "approvaltests.cpp"
-    version = "6.0.0"
+    version = "7.0.0"
     description = "Native ApprovalTests for C++ on Linux, Mac and Windows"
     topics = ("testing", "approval tests")
     url = "https://github.com/p-podsiadly/approvaltests.cpp"
@@ -17,9 +17,18 @@ class ApprovalTestsCppConan(ConanFile):
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
 
+    _header_url = "https://github.com/approvals/ApprovalTests.cpp/releases/download/v.7.0.0/ApprovalTests.v.7.0.0.hpp"
+    _header_sha256 = "9dd54bcf46171b29cdabbd1d2ee8be0ecf7cd3356379f2a309cfb16696ecc75c"
+
     def source(self):
-        tools.download(self.conan_data["sources"][self.version]["url"], "approvals/ApprovalTests.v%s.hpp" % self.version)
-        tools.check_sha256("approvals/ApprovalTests.v%s.hpp" % self.version, self.conan_data["sources"][self.version]["sha256"])
+        tools.download(self._header_url, "approvals/ApprovalTests.v%s.hpp" % self.version)
+        tools.check_sha256("approvals/ApprovalTests.v%s.hpp" % self.version, self._header_sha256)
+
+        tools.replace_in_file(
+            "approvals/ApprovalTests.v%s.hpp" % self.version,
+            "#include <doctest.h>\n",
+            "#include <doctest/doctest.h>\n",
+            strict=True)
 
         os.rename("ApprovalTests.hpp.in", "approvals/ApprovalTests.hpp")
         tools.replace_in_file("approvals/ApprovalTests.hpp", "VERSION", self.version)
